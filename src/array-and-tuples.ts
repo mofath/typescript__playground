21e758b478ed794d5f722259e1cf80f105e953db
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-10-08 06:41:34 +0200

    Added array and tuple

---
+++ src/array-and-tuples.ts
@@ -0,0 +1,12 @@
+// Array
+// Array amy have any length
+let arr: number[] = [1, 2, 3];
+
+// Tubles
+// Tuble is Array with fixed length
+// May be used to represent a point in the space
+let tuple: [number, number] = [1, 2];
+
+tuple = [1, 2];
+// tuple = [5]; // compile time error: must be 2 items
+// tuple = [5, 4 3]; // compile time error: must be 2 items
