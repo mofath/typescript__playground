707f578aac151efa501db36df83aec5d301843df
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-10-08 06:20:39 +0200

    Added primitive types

---
+++ src/primitive-types.ts
@@ -0,0 +1,9 @@
+let isPresent: boolean = false;
+let magic: number = 6.66;
+let msg: string = 'hello, world';
+
+let notDifined: undefined = undefined;
+let notPresent: null = null;
+
+let penta: symbol = Symbol('star');
+let biggy: bigint = 24n;
