211bbb6d6a647d3df4d11a6148bafb3b72af1334
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-10-08 20:18:46 +0200

    Added type declaration

---
+++ src/type-declaration.d.ts
@@ -0,0 +1,7 @@
+// typescript provides ability to declare any variable that does not present within the typescript code base
+
+declare const float: any;
+
+// You cannot provide any implemintation details
+// as they are not declaration rathe a difination
+// declare const float: any = '56.5'; // error
